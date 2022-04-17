b709da956fba402b0d4248d036e6341ac0062a63
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-04-17 12:59:11 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -84,21 +84,17 @@ resource "aws_security_group" "ssh_from_office" {
   vpc_id      = aws_vpc.my-vpc.id
 
   ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
   }
 
   egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
   }
 }
 
